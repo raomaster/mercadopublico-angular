b62bdc3a257eff0ccd77a0df6b6da48b1e316a69
Unknown <raomaster@gmail.com>
2018-07-03 02:57:50 -0400

    Merge branch 'master' into mercadopublico/master
    CRGr organismos públicos desde base de datos firebase

--- apps/mercadopublico/src/environments/environment.prod.ts
+++ apps/mercadopublico/src/environments/environment.prod.ts
@@ -1,3 +1,23 @@
 export const environment = {
-  production: true
+  production: true,
+  currency: {
+    token: 'af037fa2d0c94318930b432fc2704f56',
+    baseUrl: 'https://openexchangerates.org/api/historical/'
+  },
+  firebase: {
+    apiKey: 'AIzaSyDIQglX_XzCoJfTnVPjGES54IATnSd6btk',
+    authDomain: 'mercadopublico-8dcc7.firebaseapp.com',
+    databaseURL: 'https://mercadopublico-8dcc7.firebaseio.com',
+    projectId: 'mercadopublico-8dcc7',
+    storageBucket: 'mercadopublico-8dcc7.appspot.com',
+    messagingSenderId: '302274680042'
+  },
+  mercadoPublico: {
+    token: '47B7E7D3-5466-4459-B826-5A6FE8A4C520',
+    baseUrl: 'http://api.mercadopublico.cl/servicios/v1/publico/'
+  },
+  sbif: {
+    token: 'aa978da4dc1db427283227cd1603a81ddc91db11',
+    baseUrl: 'https://api.sbif.cl/api-sbifv3/recursos_api/'
+  }
 };
